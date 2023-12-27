26a06a158fe542000a9fbd379e44bcfd2ad5045c
Codey Huntting <creativecodey@gmail.com>
2023-12-27 14:42:34 -0600

    added gravity well and receiver

---
+++ Assets/GravityToy/Scripts/GravityReceiver.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace GravityToy
+{
+    public class GravityReceiver : MonoBehaviour
+    {
+        [SerializeField]
+        private Rigidbody2D _rigidbody;
+
+        public void ReceiveInfluence(GravityWell well)
+        {
+            Vector3 toWell = well.Position - transform.position;
+            _rigidbody.AddForce(toWell.normalized * well.Strength);
+        }
+    }
+}
--- Assets/GravityToy/Scripts/GravityWell.cs
+++ Assets/GravityToy/Scripts/GravityWell.cs
@@ -6,6 +6,29 @@ namespace GravityToy
 {
     public class GravityWell : MonoBehaviour
     {
+        public float Strength => _strength;
+        public Vector3 Position => transform.position;
 
+        [SerializeField]
+        private float _strength = 10;
+        [SerializeField]
+        private List<GravityReceiver> _receivers;
+
+        public void MoveTo(Vector3 position)
+        {
+            transform.position = position;
+        }
+
+        private void Update()
+        {
+            for (int i = 0; i < _receivers.Count; i++)
+            {
+                ExertInfluence(_receivers[i]);
+            }
+        }
+        private void ExertInfluence(GravityReceiver receiver)
+        {
+            receiver.ReceiveInfluence(this);
+        }
     }
 }
