2cc6432ff6dc91845f48420e499c821ed200a5a5
Codey Huntting <creativecodey@gmail.com>
2023-12-29 10:21:46 -0600

    Set up recoil inputs

---
+++ Assets/GunToy/Scripts/Player.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace GunToy
+{
+    public class Player : MonoBehaviour, GunToyActions.IDefaultActions
+    {
+        [SerializeField]
+        private Camera _mouseToWorldCamera;
+        [SerializeField]
+        private Reticle _reticle;
+        [SerializeField]
+        private AttackGun _attackGun;
+        [SerializeField]
+        private RecoilGun _recoilGun;
+        private GunToyActions _actions;
+
+        private void Awake()
+        {
+            _actions = new GunToyActions();
+            _actions.Default.SetCallbacks(this);
+        }
+        private void OnEnable()
+        {
+            _actions.Enable();
+        }
+        private void OnDisable()
+        {
+            _actions.Disable();
+        }
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+
+        }
+        public void OnAimPoint(InputAction.CallbackContext context)
+        {
+            Vector2 point = context.ReadValue<Vector2>();
+            point = _mouseToWorldCamera.ScreenToWorldPoint(point);
+            _reticle.SetPoint(point);
+        }
+        public void OnAimDirection(InputAction.CallbackContext context)
+        {
+            Vector2 direction = context.ReadValue<Vector2>();
+            _reticle.SetDirection(direction);
+        }
+        public void OnRecoil(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+            {
+                _recoilGun.Fire();
+            }
+        }
+    }
+}
