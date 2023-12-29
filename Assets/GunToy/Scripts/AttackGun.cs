75cfe950b9f51f217c9ccd8987f81b91c544107f
Codey Huntting <creativecodey@gmail.com>
2023-12-29 15:21:02 -0600

    setup the skeleton for targets getting hit

--- Assets/GunToy/Scripts/AttackGun.cs
+++ Assets/GunToy/Scripts/AttackGun.cs
@@ -9,9 +9,19 @@ namespace GunToy
         private MuzzleFlash _muzzleFlash;
         [SerializeField]
         private float _minimumTimeBetweenBullets = 0.1f;
+        [SerializeField]
+        private float _range = 10;
 
         public void Fire()
         {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up, _range);
+            if (hit.transform != null)
+            {
+                if (hit.transform.TryGetComponent(out Target target))
+                {
+                    target.ReceiveHit(hit);
+                }
+            }
             _muzzleFlash.Play();
         }
     }
--- Assets/GunToy/Scripts/Target.cs
+++ Assets/GunToy/Scripts/Target.cs
@@ -5,6 +5,9 @@ namespace GunToy
 {
     public class Target : MonoBehaviour
     {
-        
+        public void ReceiveHit(RaycastHit2D hit)
+        {
+
+        }
     }
 }
