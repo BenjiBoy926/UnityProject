a772135a849897ca0c1fd1dcc9a6aefec457e042
Codey Huntting <creativecodey@gmail.com>
2024-01-01 08:50:54 -0600

    lateral movement works

---
+++ Assets/SkyRailToy/Scripts/Hero.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace SkyRailToy
+{
+    public class Hero : MonoBehaviour
+    {
+        [SerializeField]
+        private HeroStats _stats;
+        [SerializeField]
+        private Rigidbody2D _rigidbody;
+        [SerializeField]
+        private int _lateralDirection;
+        [SerializeField]
+        private bool _isJumping;
+        private float _timeOfJumpStart;
+
+        private void Update()
+        {
+            UpdateLateralMovement();
+            UpdateJump();
+        }
+        private void UpdateLateralMovement()
+        {
+            Vector2 currentVelocity = _rigidbody.velocity;
+            currentVelocity.x = _lateralDirection * _stats.MoveSpeed;
+            _rigidbody.velocity = currentVelocity;
+        }
+        private void UpdateJump()
+        {
+
+        }
+    }
+}
---
+++ Assets/SkyRailToy/Scripts/HeroStats.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace SkyRailToy
+{
+    [CreateAssetMenu(menuName = "Sky Rail Toy/Hero Stats")]
+    public class HeroStats : ScriptableObject
+    {
+        public float MoveSpeed => _moveSpeed;
+        public float JumpSpeed => _jumpSpeed;
+        public float MaxJumpTime => _maxJumpTime;
+
+        [SerializeField]
+        private float _moveSpeed = 5;
+        [SerializeField]
+        private float _jumpSpeed = 5;
+        [SerializeField]
+        private float _maxJumpTime = 1;
+    }
+}
