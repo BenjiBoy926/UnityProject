6b4bfbd72827fb0a193609e13df222da3cb050bb
Codey Huntting <creativecodey@gmail.com>
2024-01-01 08:42:23 -0600

    started the sky rail toy

---
+++ Assets/SkyRailToy/Scripts/SkyObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace SkyRailToy
+{
+    public class SkyObject : MonoBehaviour
+    {
+        public Vector3 CatchPosition => _catchPosition.position;
+
+        [SerializeField]
+        private Transform _catchPosition;
+    }
+}
---
+++ Assets/SkyRailToy/Scripts/SkyRail.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace SkyRailToy
+{
+    public class SkyRail : MonoBehaviour
+    {
+        [SerializeField]
+        private SkyObject _target;
+
+        private void OnValidate()
+        {
+            ReflectTarget();
+        }
+        private void OnEnable()
+        {
+            ReflectTarget();
+        }
+
+        public void Catch(SkyObject target)
+        {
+            SetTarget(target);
+        }
+        public void Release()
+        {
+            SetTarget(null);
+        }
+
+        private void SetTarget(SkyObject target)
+        {
+            if (target == _target)
+            {
+                return;
+            }
+            _target = target;
+            ReflectTarget();
+        }
+        private void ReflectTarget()
+        {
+            if (_target != null)
+            {
+                transform.position = _target.CatchPosition;
+            }
+        }
+    }
+}
