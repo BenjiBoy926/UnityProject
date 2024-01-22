3f4e2f24b38c76b0a8b305eb7f4dc630fb457f01
Codey Huntting <creativecodey@gmail.com>
2024-01-22 07:44:19 -0600

    object on grid can animate to position

--- Assets/TurnBasedStrategyToy/Scripts/ObjectOnGrid.cs
+++ Assets/TurnBasedStrategyToy/Scripts/ObjectOnGrid.cs
@@ -1,21 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 namespace TurnBasedStrategyToy
 {
     public class ObjectOnGrid : MonoBehaviour
     {
+        private Vector3 IntendedWorldPosition => GridPositionToWorldPosition(_position);
+
         [SerializeField]
         private Vector2Int _position;
+        [SerializeField]
+        private Ease _moveEase = Ease.OutQuint;
+        [SerializeField]
+        private float _moveDuration = 1;
 
         private void OnValidate()
         {
-            SnapToPosition();
+            if (Application.isPlaying)
+            {
+                AnimateToPosition();
+            }
+            else
+            {
+                SnapToPosition();
+            }
+        }
+        private void AnimateToPosition()
+        {
+            transform.DOKill();
+            transform.DOMove(IntendedWorldPosition, _moveDuration);
         }
         private void SnapToPosition()
         {
-            transform.position = GridPositionToWorldPosition(_position);
+            transform.position = IntendedWorldPosition;
         }
         private static Vector3 GridPositionToWorldPosition(Vector2Int gridPosition)
         {
