0d4a63e8258877a0935e0d2a1e949efa955fc35a
Codey Huntting <creativecodey@gmail.com>
2024-01-28 14:21:32 -0600

    worked out the stub a little more

--- Assets/TurnBasedStrategyToy/Scripts/BattleUnit.cs
+++ Assets/TurnBasedStrategyToy/Scripts/BattleUnit.cs
@@ -14,25 +14,26 @@ namespace TurnBasedStrategyToy
 
         public void AnimateTo(Vector2Int position)
         {
+            position = ClampInMovement(position);
             _self.AnimateTo(position);
         }
         public Vector2Int ClampInMovement(Vector2Int position)
         {
             Vector2Int delta = position - _self.GridPosition;
-            delta = ClampSum(delta, _stats.Movement);
+            delta = ClampAbsoluteSum(delta, _stats.Movement);
             return _self.GridPosition + delta;
         }
-        private Vector2Int ClampSum(Vector2Int vector, int maxSum)
+        private Vector2Int ClampAbsoluteSum(Vector2Int vector, int maxSum)
         {
-            while (Sum(vector) > maxSum)
+            if (AbsoluteSum(vector) <= maxSum)
             {
-
+                return vector;
             }
             return vector;
         }
-        private int Sum(Vector2Int vector)
+        private int AbsoluteSum(Vector2Int vector)
         {
-            return vector.x + vector.y;
+            return Mathf.Abs(vector.x) + Mathf.Abs(vector.y);
         }
         private void OnDrawGizmosSelected()
         {
