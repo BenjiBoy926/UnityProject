301f161470bfd54df6ccbf2d6ba6e1a812fd8786
Codey Huntting <creativecodey@gmail.com>
2024-01-28 14:39:49 -0600

    started an algorithm with a bad tiebreaker

--- Assets/TurnBasedStrategyToy/Scripts/BattleUnit.cs
+++ Assets/TurnBasedStrategyToy/Scripts/BattleUnit.cs
@@ -12,12 +12,39 @@ namespace TurnBasedStrategyToy
         private BattleUnitStats _stats;
         [SerializeField]
         private ObjectOnGrid _self;
+        private List<Vector2Int> _reachableTiles = new List<Vector2Int>();
 
         public void AnimateTo(Vector2Int position)
         {
             position = ClampInMovement(position);
             _self.AnimateTo(position);
         }
+
+        public Vector2Int ClampInMovement(Vector2Int position)
+        {
+            MovementReachableTiles(_reachableTiles);
+            if (_reachableTiles.Count == 0)
+            {
+                return position;
+            }
+            int shortestIndex = 0;
+            int shortestDistance = int.MaxValue;
+            for (int i = 0; i < _reachableTiles.Count; i++)
+            {
+                int distance = RectangularDistance(position, _reachableTiles[i]);
+                if (distance < shortestDistance)
+                {
+                    shortestIndex = i;
+                    shortestDistance = distance;
+                }
+            }
+            return _reachableTiles[shortestIndex];
+        }
+        private int RectangularDistance(Vector2Int a, Vector2Int b)
+        {
+            return Mathf.Abs(b.x - a.x) + Mathf.Abs(b.y - a.y);
+        }
+
         private void MovementReachableTiles(List<Vector2Int> tiles)
         {
             Assert.IsNotNull(tiles);
@@ -37,29 +64,15 @@ namespace TurnBasedStrategyToy
                 }
             }
         }
-        public Vector2Int ClampInMovement(Vector2Int position)
-        {
-            Vector2Int delta = position - _self.GridPosition;
-            delta = ClampAbsoluteSum(delta, _stats.Movement);
-            return _self.GridPosition + delta;
-        }
-        private Vector2Int ClampAbsoluteSum(Vector2Int vector, int maxSum)
-        {
-            if (AbsoluteSum(vector) <= maxSum)
-            {
-                return vector;
-            }
-            return vector;
-        }
         private int AbsoluteSum(Vector2Int vector)
         {
             return Mathf.Abs(vector.x) + Mathf.Abs(vector.y);
         }
+
         private void OnDrawGizmosSelected()
         {
-            List<Vector2Int> reachable = new List<Vector2Int>();
-            MovementReachableTiles(reachable);
-            foreach (Vector2Int position in reachable)
+            MovementReachableTiles(_reachableTiles);
+            foreach (Vector2Int position in _reachableTiles)
             {
                 Gizmos.DrawWireCube(_self.GridToWorld(position), Vector3.one);
             }
